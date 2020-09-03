f585593f18cd350c9b1261e1b088bbed97b45865
MinChan Lee <shigatsu970704@gmail.com>
2020-09-03 17:15:04 +0900

    5.Fix Context and Recap

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 500;
+canvas.height = 500;
+
 ctx.strokestyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -18,10 +21,10 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    ctx.beginpath();
+    ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
-    ctx.lineTo();
+    ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
